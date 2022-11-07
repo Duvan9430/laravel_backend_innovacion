cb06a117ca403a55cbc846c9d0575bdcf5f2314f
Duban <duvan9430@gmail.com>
2022-11-07 16:49:50 -0500

    fix:agregando preguntas  y repsuestas

--- app/Http/Controllers/API/VideosController.php
+++ app/Http/Controllers/API/VideosController.php
@@ -7,6 +7,8 @@ use Dingo\Api\Http\Response;
 use Illuminate\Http\Request;
 use Log;
 use App\Models\VideoUrl;
+use App\Models\Preguntas;
+use App\Models\Respuestas;
 use DB;
 use Hash;
 use Carbon\Carbon;
@@ -15,7 +17,7 @@ use App\Helpers\helpers;
 use App\Exceptions\CustomException;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Collection;
 
 class VideosController extends Controller
 {
@@ -25,11 +27,25 @@ class VideosController extends Controller
         $this->helper = $helper;
     }
 
-   public function obtenerVideo($idVideo){
-        $videos = VideoUrl::withoutTrashed()
-                  ->join('temas as tem','tem.id','url_videos.temas_id')
-                  ->where('temas_id',$idVideo)->get();
-        return response()->json($videos);
-   }
+    public function obtenerVideo($idVideo){
+            $videos = VideoUrl::withoutTrashed()
+                    ->join('temas as tem','tem.id','url_videos.temas_id')
+                    ->where('temas_id',$idVideo)->get();
+            return response()->json($videos);
+    }
+    public function obtenerVideoPreguntas($idVideo){
+
+         $preguntas = Preguntas::withoutTrashed()
+                ->join('respuestas as res','res.preguntas_id','preguntas.id')
+                ->where('temas_id',$idVideo)
+                ->select('preguntas.preguntas','preguntas.temas_id','res.preguntas_id')
+                ->groupBy('preguntas.temas_id','preguntas.preguntas','res.preguntas_id')->get();
+        return response()->json($preguntas);
+    }
+    public function obtenerVideoPreguntasRespuestas($idsPreguntas){
+        $respuestas = Respuestas::withoutTrashed()
+                ->whereIn('preguntas_id',explode(",",$idsPreguntas))->get();
+        return response()->json($respuestas);
+    }
 
 }
